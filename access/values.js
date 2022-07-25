341cc7411820e0ba9851c3d646abff1633e72d79
zabeu700 <105270553+zabeu700@users.noreply.github.com>
2022-07-25 14:37:18 +0300

    Add files via upload

--- access/values.js
+++ access/values.js
@@ -3,15 +3,15 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "03.01.2004"; // Дата рождения
-var fullname = "Хрящів Константин Юрійович" // ФИО
+var birthdate = "23.03.2004"; // Дата рождения
+var fullname = "Заведій Дмитро Іванович" // ФИО
 var name = "Имя" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "176457369" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "0173656711" // ИНН
 
 // Права
 var rights_valid_until = "11.09.2023" // Права "Дійсні до"
@@ -20,5 +20,5 @@ var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "09.02.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "09.02.2023" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
